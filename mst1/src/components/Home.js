81d138546e18047d2deba6c2a6002f4fbff09096
Maharishi1534 <gauravsharma910415344@gmail.com>
2026-03-11 14:02:51 +0530

    Restore all experiments

---
+++ mst1/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+
+      <h2>Welcome to University Portal</h2>
+
+      <p>Select a course to view details.</p>
+
+    </div>
+  );
+}
+
+export default Home;
